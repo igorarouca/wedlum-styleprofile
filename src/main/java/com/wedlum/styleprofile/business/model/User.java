9d273800eed2ab517e8c451a81f6c7f8c5a18f0c
igorarouca <igor.arouca@acm.org>
2013-06-14 08:28:07 -0500

    SerialUID changed to 1l

--- src/main/java/com/wedlum/styleprofile/business/model/User.java
+++ src/main/java/com/wedlum/styleprofile/business/model/User.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class User implements Serializable {
 
-	private static final long serialVersionUID = -6499854753866232154L;
+	private static final long serialVersionUID = 1L;
 
 	private String name;
 	private String address;
