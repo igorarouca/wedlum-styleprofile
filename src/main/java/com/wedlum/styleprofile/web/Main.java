77219ca8280f945f84b4e5dce0e8e22dde397ac4
kalecser <kalecser@gmail.com>
2013-06-23 13:16:49 +0100

    Accept weboot as commandline argument to main class

--- src/main/java/com/wedlum/styleprofile/web/Main.java
+++ src/main/java/com/wedlum/styleprofile/web/Main.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         PhotoSourceUsingFileSystem.STORAGE = new File("src/main/webapp/photo-storage");
-        Launcher.main("--webroot src/main/webapp".split(" "));
+        String webroot = (args.length >0)?args[0]:"src/main/webapp";
+        Launcher.main(("--webroot " + webroot).split(" "));
     }
 }
