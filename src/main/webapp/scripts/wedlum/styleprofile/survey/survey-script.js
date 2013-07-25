0630419a71cdd3a759a6795b10a3091cbf2a30cd
igorarouca <igor.arouca@acm.org>
2013-07-25 16:53:16 -0500

    wedlum.css cleaned-up and formatted

--- src/main/webapp/scripts/wedlum/styleprofile/survey/survey-script.js
+++ src/main/webapp/scripts/wedlum/styleprofile/survey/survey-script.js
@@ -7,8 +7,8 @@ wedlum.styleprofile.survey.script = [
         name : "singleColorSession1",
         type: "session",
         data: [
-            "000M.png",  "015M.png",  "035M.png",  "045M.png",  "060M.png",  "075M.png",
-            "100M.png",  "135M.png",  "170M.png",  "200M.png", "220M.png", "250M.png",
+            "000M.png", "015M.png", "035M.png", "045M.png", "060M.png", "075M.png",
+            "100M.png", "135M.png", "170M.png", "200M.png", "220M.png", "250M.png",
             "280M.png", "300M.png", "330M.png", "345M.png", "BL00.png", "WH00.png"
         ]
     },
@@ -16,18 +16,18 @@ wedlum.styleprofile.survey.script = [
         name : "singleColorSession2",
         type: "session",
         data: [
-             "000S.png",  "000N.png",  "035S.png",  "035N.png",  "060S.png",  "060N.png",
-            "100S.png",  "100N.png",  "170S.png",  "170N.png", "220S.png", "220N.png",
-            "280S.png", "280N.png", "330S.png", "330N.png", "LG00.png", "DG00.png"
+             "000S.png", "000N.png", "035S.png", "035N.png", "060S.png", "060N.png",
+             "100S.png", "100N.png", "170S.png", "170N.png", "220S.png", "220N.png",
+             "280S.png", "280N.png", "330S.png", "330N.png", "LG00.png", "DG00.png"
         ]
     },
     {
         name : "singleColorSession3",
         type: "session",
         data: [
-                 "000L.png",  "000D.png",  "035L.png",  "035D.png",  "060L.png",  "060D.png",
-            "100L.png",  "100D.png",  "170L.png",  "170D.png", "220L.png", "220D.png",
-            "280L.png", "280D.png", "330L.png", "330D.png", "WG00.png", "CG00.png"
+             "000L.png", "000D.png", "035L.png", "035D.png", "060L.png", "060D.png",
+             "100L.png", "100D.png", "170L.png", "170D.png", "220L.png", "220D.png",
+             "280L.png", "280D.png", "330L.png", "330D.png", "WG00.png", "CG00.png"
         ]
     },
     {
