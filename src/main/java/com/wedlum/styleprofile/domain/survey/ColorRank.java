9604315df1fa58926a3357dbe1629fbee863a544
igorarouca <igor.arouca@acm.org>
2013-08-07 08:20:27 -0500

    ColorRank WIP

--- src/main/java/com/wedlum/styleprofile/domain/survey/ColorRank.java
+++ src/main/java/com/wedlum/styleprofile/domain/survey/ColorRank.java
@@ -1,15 +1,38 @@
 package com.wedlum.styleprofile.domain.survey;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
 class ColorRank {
 
-	static List<String> rankColors(List<String> likes, List<String> allColors) {
-		// B3/A3*($E$2^(B3^2))*10
+	static List<String> rankColors(final List<String> likes, List<String> allColors) {
 		// (Liked / Times Shown) * (Magic_Factorö(Likedö2)) * 10
 		List<String> result = unique(likes);
+
+		Collections.sort(result, new Comparator<String>() {
+
+			int score(String color) {
+				count(color, likes);
+				return 0;
+			}
+			
+			private int count(String item, List<String> list) {
+				int count = 0;
+				for (String each : list)
+					if (each.equals(item)) ++count;
+
+				return count;
+			}
+
+			@Override public int compare(String color1, String color2) {
+				return score(color1) - score(color2);
+			}
+
+		});
+
 		return result;
     }
 
